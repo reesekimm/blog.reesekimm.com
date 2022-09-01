f3beaaf8622ccb82c784afa4516d8521cfb69cb9
reesekimm <reesekimm@gmail.com>
2022-09-01 15:48:22 +0900

    feat: home 테스트 작성
    TODO: 리스트 아이템 클릭시 페이지 이동 E2E 테스트

---
+++ src/pages/__tests__/home.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { PureHome as Home } from '..'
+import { render, screen } from '../../utils/testing-library-util'
+
+const data = {
+  allMdx: {
+    nodes: [
+      {
+        frontmatter: {
+          date: 'April 5, 2022',
+          title: '실용주의 프로그래머 09',
+          subtitle: null,
+          slug: 'pragmatic-programmer-09',
+        },
+        id: '0dcbeb7d-de6f-57ec-b302-9ab52fb9877b',
+      },
+      {
+        frontmatter: {
+          date: 'April 3, 2022',
+          title: '실용주의 프로그래머 08',
+          subtitle: '노마드코더 북클럽 실용주의 프로그래머 1기에 참여하며 남기는 기록',
+          slug: 'pragmatic-programmer-08',
+        },
+        id: 'a94d7c73-c694-5b8e-a882-568fddf8796f',
+      },
+      {
+        frontmatter: {
+          date: 'April 2, 2022',
+          title: '실용주의 프로그래머 07',
+          subtitle: null,
+          slug: 'pragmatic-programmer-07',
+        },
+        id: 'df1deb2c-43c2-5cf1-8cb2-c730b6a74715',
+      },
+    ],
+  },
+}
+
+describe('Home', () => {
+  it('should display post list', () => {
+    render(<Home data={data} />)
+
+    const listItems = screen.getAllByRole('listitem')
+
+    expect(listItems).toHaveLength(3)
+  })
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,40 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 import ListItem from '../components/listItem'
 
-const Home = ({ data }) => {
+export const PureHome = ({ data }) => {
   return (
-    <Layout pageTitle="All Posts">
-      <ul style={{ listStyle: 'none', margin: 0, padding: 0 }}>
-        {data.allMdx.nodes.map(({ id, frontmatter: { date, title, subtitle, slug } }) => (
-          <ListItem key={id} path={`/posts/${slug}`} title={title} subtitle={subtitle} date={date} />
-        ))}
-      </ul>
-    </Layout>
+    <ul style={{ listStyle: 'none', margin: 0, padding: 0 }}>
+      {data.allMdx.nodes.map(({ id, frontmatter: { date, title, subtitle, slug } }) => (
+        <ListItem key={id} path={`/posts/${slug}`} title={title} subtitle={subtitle} date={date} />
+      ))}
+    </ul>
   )
 }
 
-export const query = graphql`
-  query {
-    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-          subtitle
-          slug
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            title
+            subtitle
+            slug
+          }
+          id
         }
-        id
       }
     }
-  }
-`
+  `)
+
+  return (
+    <Layout pageTitle="All Posts">
+      <PureHome data={data} />
+    </Layout>
+  )
+}
 
 export default Home
