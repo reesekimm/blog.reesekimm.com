279d96963839469a1a872863d57bb034fa512d9b
reesekimm <reesekimm@gmail.com>
2022-09-07 13:49:22 +0900

    feat: page title 폰트 사이즈 조정

--- src/components/layout/style.ts
+++ src/components/layout/style.ts
@@ -7,8 +7,18 @@ export const StyledBody = styled.main`
   box-sizing: border-box;
   margin: 0 auto;
   padding: 12rem 2.4rem 4rem;
+
+  @media ${({ theme }) => theme.device.mobile} {
+    padding: ;
+  }
 `
 export const PageTitle = styled.h2`
-  font-size: 5rem;
-  margin-top: 0;
+  font-size: 4rem;
+  margin: 0;
+
+  @media ${({ theme }) => theme.device.mobile} {
+    font-size: 2.8rem;
+    padding: 0;
+    margin: 0;
+  }
 `
