312180cf212c6bb8c825db14c501841f3aad1c64
reesekimm <reesekimm@gmail.com>
2022-09-23 15:32:37 +0900

    refactor: 불필요한 코드 삭제

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,3 @@
-import '@fontsource/ibm-plex-sans-kr/300.css'
-import '@fontsource/ibm-plex-sans-kr/500.css'
-import '@fontsource/ibm-plex-sans-kr/700.css'
-import '@fontsource/roboto-mono/400.css'
-import '@fontsource/roboto-mono/400-italic.css'
-
 import Layout from './src/components/layout'
 import Wrapper from './src/components/wrapper'
 
