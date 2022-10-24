2e5b6841006a3913388a195404072bc07be8c997
reesekimm <reesekimm@gmail.com>
2022-10-24 16:15:55 +0900

    feat: header transition 기능 적용

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,12 +3,28 @@ import { Link } from 'gatsby'
 import { Box, Flex, Heading, Spacer } from '@chakra-ui/react'
 import useSiteMetadata from '../../hooks/useSiteMetadata'
 
-export const Header = () => {
+interface HeaderProps {
+  showing: boolean
+}
+
+export const Header = ({ showing }: HeaderProps) => {
   const { title } = useSiteMetadata()
 
   return (
-    <Flex as="header" alignItems="center" p="4">
-      <Box>
+    <Flex
+      as="header"
+      alignItems="center"
+      bg="white"
+      p="4"
+      position="fixed"
+      w="100%"
+      left={0}
+      transform={showing ? 'translateY(0)' : 'translateY(-5rem)'}
+      transition="transform 0.3s"
+      zIndex={10}
+      boxShadow="sm"
+    >
+      <Box zIndex={10}>
         <Heading as="h2" size="md" p={0}>
           <Link to="/">{title}</Link>
         </Heading>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,17 +5,26 @@ import Footer from './footer'
 import SEO from './seo'
 import { ChakraProvider, Container } from '@chakra-ui/react'
 import theme from '../styles/theme'
+import useHeaderTransition from '../hooks/useHeaderTransition'
 
 interface LayoutProps {
   element: GatsbyBrowser['wrapPageElement'] | GatsbySSR['wrapPageElement']
 }
 
 const Layout = ({ element }: LayoutProps) => {
+  const { headerShowing } = useHeaderTransition()
+
   return (
     <ChakraProvider theme={theme}>
       <>
-        <Header />
-        <Container as="main" maxW="75rem" flex="1" centerContent>
+        <Header showing={headerShowing} />
+        <Container
+          as="main"
+          maxW="75rem"
+          flex="1"
+          marginTop="4rem"
+          centerContent
+        >
           <>{element}</>
         </Container>
         <Footer />
