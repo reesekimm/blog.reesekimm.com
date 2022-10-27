984bb8fb4c991068798c4506c2429879c7af68a4
reesekimm <reesekimm@gmail.com>
2022-10-27 18:45:19 +0900

    fix: styled components 의존성 제거

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,27 +1,10 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import styled from 'styled-components'
-
-export const Container = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  margin-top: 24rem;
-
-  a {
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`
+import { Container } from '@chakra-ui/react'
 
 const NotFound = () => {
   return (
-    <Container>
-      <h2>Page not found</h2>
-      <Link to="/">Back to home</Link>
+    <Container as="section" centerContent>
+      <h2>존재하지 않는 페이지 입니다.</h2>
     </Container>
   )
 }
